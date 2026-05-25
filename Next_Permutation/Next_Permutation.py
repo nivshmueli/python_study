fc595a80e913f49a1ec0f41c565cfa8042a19ab5
nivshmueli <niv.shmueli@gmail.com>
2026-05-25 09:44:16 -0400

    first version

---
+++ Next_Permutation/Next_Permutation.py
@@ -0,0 +1,23 @@
+
+import numpy as np
+
+def next_permutation(perm_in):
+
+    perm_in = np.array(perm_in)
+
+    last=len(perm_in-1)
+
+
+    for i in range(last,1,-1):
+        if perm_in[i-1] < perm_in[i]:
+            # in the range i to the end
+            #fsort up the range perm_in[i-1 to end]
+            sorted_left = sort_up(perm_in[i-1:-1])
+
+            # find  perm_in[i-1] im=n the sorted list and thake the next location: loc
+            perm_in[i-1] = sorted_left[loc]
+
+            np.delete(sorted_left, loc)
+
+            perm_in[i:-1] = sorted_left
+        
